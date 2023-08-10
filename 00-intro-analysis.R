fc68d64cbf4be3ad3f0e83e0ca21a42b43a71b65
Michael <you@example.com>
2023-08-10 16:06:08 -0400

    Added RUCC Codes and intro bayes info

--- 00-intro-analysis.R
+++ 00-intro-analysis.R
@@ -7,15 +7,30 @@ library(cowplot)
 
 # Poverty Data (DP03_119PE)
 # Vacant housing units (DP04_0003PE)
-# Percent!!EMPLOYMENT STATUS!!Civilian labor force!!Unemployment Rate
-ga_map_data <- data_readin(acs_vars = "DP03_0119PE,DP04_0003PE,DP03_0009PE")
+# Percent!!EMPLOYMENT STATUS!!Civilian labor force!!Unemployment Rate (DP03_0009PE)
+ga_map_data <- data_readin(acs_vars = "DP03_0119PE,DP04_0003PE,DP03_0009PE", acs_yr = 2019)
 
 ## Bivariate-----------------------------------------------
 
-Hmisc::rcorr(as.matrix(ga_map_data[,c("pct_poverty", "vacancy_rate", "unemployment_rate")]))
+# Hmisc::rcorr(as.matrix(ga_map_data[,c("pct_poverty", "vacancy_rate", "unemployment_rate")]))
 
 ggplot(data = ga_map_data, aes(x = vacancy_rate, y = unemployment_rate)) +
   geom_point()
+
+ggplot(data = ga_map_data, aes(x = vacancy_rate, y = pct_poverty)) +
+  geom_point() 
+
+ga_map_data%>% st_drop_geometry() %>% 
+  pivot_longer(cols = c(pct_poverty, vacancy_rate, unemployment_rate)) %>% 
+  ggplot(aes(x = value, y = Incidence)) +
+  geom_point(alpha = 1 / 3) +
+  geom_smooth(colour = "black", span = .5) +
+  theme_bw() +
+  labs(x = "Percent", y = "Opioid Mortality Rate", 
+       title = "Plot of Indicator Association with Incidence",
+       subtitle = "for GA Counties") +
+  theme(guide = NULL) +
+  facet_grid(~name, scales = "free_x")
 
 # Bayes---------------------------------------------------
 
@@ -43,7 +58,7 @@ ps <- ggplot(ga_map_data) +
 
 print(ps)
 
-form <- "Incidence ~ pct_poverty + vacancy_rate + unemployment_rate"
+form <- "Incidence ~ pct_poverty + vacancy_rate + unemployment_rate + offset(Population)"
 
 model.spatial <- S.CARleroux(as.formula(form), family = "gaussian",
                              data = ga_map_data,
@@ -51,7 +66,13 @@ model.spatial <- S.CARleroux(as.formula(form), family = "gaussian",
                              n.sample = 100000,
                              thin = 10)
 
+model <- lm(as.formula(form), family = "gaussian",
+            data = ga_map_data)
+
 print(summary(model.spatial))
+print(model.spatial)
+print(model)
+
 model.spatial$summary.results
 
 ga_map_data <- mutate(ga_map_data,
@@ -72,12 +93,85 @@ ggplot(test_vals, aes(x = id, y = value, group = name, colour = name)) +
 
 p1 <- ggplot(ga_map_data) +
   geom_sf(aes(fill = Incidence)) +
-  scale_fill_viridis() +
-  labs("Unsmoothed Incidence")
+  scale_fill_viridis() 
 
 p2 <- ggplot(ga_map_data) +
   geom_sf(aes(fill = fitted_values)) +
-  scale_fill_viridis() +
-  labs("Smoothed Incidence")
+  scale_fill_viridis() 
+
+plot_grid(p1, p2, labels = c("Unsmoothed", "Smoothed"))
+
+#-------------------------------------------------------------------
+
+# Same with rucc codes
+ggplot(ga_map_data) +
+  geom_sf(aes(fill = rucc_code13)) +
+  scale_fill_viridis(discrete = T, direction = -1) +
+  labs(title = "NCHS Urban Rural Classification")
+
+ga_map_data %>% 
+  st_drop_geometry() %>% 
+  count(rucc_code13) %>% mutate(prop = n / sum(n) * 100)
+
+ga_map_data %>% 
+  st_drop_geometry() %>% 
+  group_by(rucc_code13) %>% 
+  ggplot(aes(x = rucc_code13, y = Incidence)) +
+  geom_boxplot() +
+  labs(title = "Opioid Mortality Incidence by Urban Rural",
+       subtitle = "Large Central, Large Fringe, Medium Metro, Small Metro, Micrro, Non-Core",
+       x = "URCC Code",
+       y = "Incidence")
+  
+
+form <- "Incidence ~ rucc_code13 + pct_poverty + vacancy_rate + unemployment_rate+ offset(Population)"
+
+model.spatial <- S.CARleroux(as.formula(form), family = "gaussian",
+                             data = ga_map_data,
+                             W = W_mt, burnin = 20000,
+                             n.sample = 100000,
+                             thin = 10)
+
+model.spatial.chain2 <- S.CARleroux(as.formula(form), family = "gaussian",
+                             data = ga_map_data,
+                             W = W_mt, burnin = 20000,
+                             n.sample = 100000,
+                             thin = 10)
+
+# Need to add comparison chain
+plot(mcmc.list(model.spatial$samples$beta, model.spatial.chain2$samples$beta)[,2:3])
+
+
+model <- lm(as.formula(form), family = "gaussian",
+            data = ga_map_data)
+
+print(summary(model.spatial))
+print(model.spatial)
+print(model)
+
+model.spatial$summary.results
+
+ga_map_data <- mutate(ga_map_data,
+                      fitted_values = model.spatial$fitted.values)
+
+ggplot(ga_map_data, aes(x = Incidence, y = fitted_values)) +
+  geom_point() +
+  theme_bw()
+
+test_vals <- tibble(id = 1:length(model.spatial$fitted.values), 
+                    fitted_inc = model.spatial$fitted.values,
+                    true_inc = ga_map_data$Incidence) %>% 
+  pivot_longer(c(fitted_inc, true_inc))
+
+ggplot(test_vals, aes(x = id, y = value, group = name, colour = name)) +
+  geom_point() +
+  theme_bw()
+
+p1 <- ggplot(ga_map_data) +
+  geom_sf(aes(fill = Incidence)) +
+  scale_fill_viridis()
+p2 <- ggplot(ga_map_data) +
+  geom_sf(aes(fill = fitted_values)) +
+  scale_fill_viridis() 
 
 plot_grid(p1, p2, labels = c("Unsmoothed", "Smoothed"))
--- Helper_Functions/data-readin.R
+++ Helper_Functions/data-readin.R
@@ -1,4 +1,4 @@
-data_readin <- function(acs_vars = "DP03_0119PE,DP04_0003PE,DP03_0009PE"){
+data_readin <- function(acs_vars = "DP03_0119PE,DP04_0003PE,DP03_0009PE", acs_yr = 2021){
   # data readin
   require(readxl)
   require(dplyr)
@@ -7,7 +7,7 @@ data_readin <- function(acs_vars = "DP03_0119PE,DP04_0003PE,DP03_0009PE"){
   
   ##-Predictors------------------------------------------------------------------
 
-  acs <- get_acs(2021, acs_vars = acs_vars, resolution = "county")
+  acs <- get_acs(acs_yr = acs_yr, acs_vars = acs_vars, resolution = "county")
   
   # acs %>% 
   #   filter(state == "13") %>% count()
@@ -21,7 +21,8 @@ data_readin <- function(acs_vars = "DP03_0119PE,DP04_0003PE,DP03_0009PE"){
       pct_poverty = as.numeric(DP03_0119PE),
       vacancy_rate = as.numeric(DP04_0003PE),
       unemployment_rate = as.numeric(DP03_0009PE)
-    )
+    ) %>% 
+    rename
   
   ##-Opioid----------------------------------------------------------------------
   
@@ -31,14 +32,37 @@ data_readin <- function(acs_vars = "DP03_0119PE,DP04_0003PE,DP03_0009PE"){
     right_join(acs_georgia, by = c("County" = "NAME")) %>% 
     rename(NAME = County)
   
+  ##-Data Readin-----------------------------------------------------------------
+  
+  rucc <- read_xlsx(here::here("Data/NCHS/rucc_codes/NCHSURCodes2013.xlsx")) %>%
+    filter(`State Abr.` == "GA") %>% 
+    mutate(county = substr(as.character(`FIPS code`),3,5),
+           rucc_code13 = factor(`2013 code`, labels = c("lcm","lfm","mm","sm","mi","non"))) %>% 
+    select(county, rucc_code13, `2013 code`) 
+  
+  # rucc %>% count(rucc_code13, `2013 code`)
+    
+  full_data_codes <- rucc %>% right_join(full_data)
+    
   ##-Map-------------------------------------------------------------------------
   
   ga_map <- st_read(here::here("Data/Map/County.shp"),
                     quiet = T)
   
   ga_map_data <- ga_map %>% 
-    left_join(full_data) %>% 
-    mutate(Incidence = Mortality / Population)
+    left_join(full_data_codes) %>% 
+    mutate(incidence = Mortality / Population) %>% 
+    rename(mortality = Mortality,
+           population = Population)
+  
+  ##-Standardize Variables------------------------------------------------------
+  
+  standardize <- function(x) (x - mean(x, na.rm = T)) / sd(x, na.rm = T)
+  
+  ga_map_data <- ga_map_data %>% 
+    mutate(across(c(pct_poverty, vacancy_rate, unemployment_rate), 
+                  ~  standardize(.x),
+                  .names = "{.col}_std"))
   
   return(ga_map_data)
 
